3ec09978e0de256d5d1ffc5b80d0ae139b67627f
Азат Фарваев <afarvaev4@gmail.com>
2025-02-14 17:24:02 +0300

    add new block and footer

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
+import anime from "animejs/lib/anime.es.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
@@ -7,4 +8,40 @@ document.addEventListener("DOMContentLoaded", () => {
       startEvent: "DOMContentLoaded",
     });
   }, 500);
+});
+
+document.querySelectorAll(".faq").forEach((faq) => {
+  const header = faq.querySelector(".faq-header");
+  const body = faq.querySelector(".faq-body");
+  const icon = faq.querySelector("svg");
+
+  if (body) {
+    body.style.height = "0px";
+    body.style.overflow = "hidden";
+    body.style.transition = "height 0.4s ease-in-out";
+  }
+
+  faq.addEventListener("click", () => {
+    if (body.style.height === "0px" || body.style.height === "") {
+      body.style.height = body.scrollHeight + 10 + "px";
+      body.style.paddingTop = "10px";
+      anime({
+        targets: icon,
+        rotate: "180deg",
+        duration: 300,
+        easing: "easeInOutQuad",
+      });
+    } else {
+      body.style.height = "0px";
+      anime({
+        targets: icon,
+        rotate: "0deg",
+        duration: 300,
+        easing: "easeInOutQuad",
+        complete: () => {
+          body.style.paddingTop = "0px";
+        },
+      });
+    }
+  });
 });
